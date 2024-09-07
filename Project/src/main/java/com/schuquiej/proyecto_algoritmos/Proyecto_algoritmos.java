279ac4e58830443f9e265c8371a7dc1170316c2c
schuquiej <71852316+schuquiej@users.noreply.github.com>
2024-09-06 19:16:21 -0600

    SCHUQUIEJ_CREATE_DBS
    CREATE DATABASES

---
+++ Project/src/main/java/com/schuquiej/proyecto_algoritmos/Proyecto_algoritmos.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.schuquiej.proyecto_algoritmos;
+
+import interfaces.loginUser;
+
+/**
+ *
+ * @author sergi
+ */
+public class Proyecto_algoritmos {
+
+    public static void main(String[] args) {
+     
+        System.out.print("revisar");
+        loginUser usuarios = new loginUser();
+        usuarios.setVisible(true);
+        
+        
+    }
+}
